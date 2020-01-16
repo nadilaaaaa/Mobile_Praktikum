904a26a5589bd97a4558df5ebf6461bbcb1eec20
nadilaaaaa <nuruladila766@gmail.com>
2020-01-16 16:55:16 +0700

    ittcantek

---
+++ LokawisataBanyumas/app/src/main/java/com/example/hm_ittp/HmjData.java
@@ -0,0 +1,73 @@
+package com.example.hm_ittp;
+
+import java.util.ArrayList;
+
+public class HmjData {
+
+    public static String[][] data = new String[][] {
+            {"0",
+                    "Bukit Tranggulasih",
+                    "Bukit Tranggulasih terletak di Desa Windujaya kecamatan Kedungbanteng Banyumas, tepatnya di RT 03 RW 05, sekitar 15km dari pusat kota. Bukit ini banyak menyita perhatian wisatawan dalam maupun luar kota. Dapat ditempuh dari dua jalur, bisa dari desa Kedungbanteng ke utara lewat gerbang masuk Curug Gomblang, atau bisa juga dari kawasan wisata Curug Bayan (Curug Gede) Baturaden. Hanya dengan membayar tiket masuk parkir senilai Rp. 3000,- saja\n" +
+                            "kita sudah bisa berekreasi sepuasnya ditempat ini.",
+                    "https://i1.wp.com/landscaper.id/wp-content/uploads/2018/04/photo_2018-04-17_20-52-22.jpg?ssl=1"},
+            {"1",
+                    "Curug Bayan",
+                    "Curug Bayan ini merupakan salah satu tempat wisata air yang cukup digemari untuk semua kalangan dan usia. Tempatnya yang dekat dan mudah dijangkau, murah, pilihan kedalaman air juga bisa dipilih sesuai umur dan di pisah relatif aman. Tak salah sih kalo dijadikan salah satu tujuan wisata untuk keluarga.\n" +
+                            "Berjarak sekitar 13 Km dan bisa ditempuh selama 30 menit dengan kendaraan santai.",
+                    "https://1.bp.blogspot.com/-ZbbMJSU60qc/XHIxcFAOLfI/AAAAAAAAFEk/nsLV01nDyO486GlHKyVb5iFJttCBKLOxQCLcBGAs/s1600/curugbayan-kalipagu.jpg"},
+            {"2",
+                    "Telaga Sunyi",
+                    "Telaga Sunyi Dilihat dari nama telaga sunyi ini adalah sebuah tempat wisata di Purwokerto yang sangat indah. Di lihat dari namanya telaga ini memang sunyi dan jauh dari keramaian. Tempat wisata telaga sunyi ini tidak jauh dari lokawisata Baturraden, hanya berjarak 2,5 KM. Selain Telaga Sunyi ini juga didukung oleh pesona alam yang asri, udaranya yang sejuk dll. Cocok sekali buat anda yang ingin melepaskan penat setelah bekerja atau menjadi jadwal liburan di weekend ini. Air di Telaga Sunyi Baturraden ini juga jernih sehingga anda dapat bermain air,\n" +
+                            "berenang atau hanya sekedar berfoto dengan background pesona telaga dan alamnya.",
+                    "https://www.pegipegi.com/travel/wp-content/uploads/2018/07/foto-2.jpg"},
+            {"3",
+                    "Pagubugan Malung",
+                    "Desa Melung, Kecamatan Kedungbanteng, Kabupaten Banyumas mengusung konsep wisata unik dengan memanfaatkan sawah terasering milik desa. Lanskap kolam pemandian berlokasi diapit teras-teras sawah\n" +
+                            "dengan pemandangan bukit menjulang di bawah kolong langit biru.",
+                    "https://cdns.klimg.com/merdeka.com/i/w/foto/2019/04/11/575579/m/biru-air-pemandian-di-hijau-persawahan-melung-008-debby-restu-utomo.jpg"},
+            {"4",
+                    "Telaga Kumpe",
+                    "Telaga Kumpe merupakan obyek wisata berupa telaga yang berada di dataran tinggi tepatnya di Dusun Pesawahan, Desa Gununglurah, Cilongok. Di tempat ini juga tersedia berbagai fasilitas seperti perahu dayung, spot foto yang menarik dengan pemandangan alam khas pedesaan dan juga ada tempat untuk camping. Tidak ada biaya yang harus dibayarkan untuk masuk ke tempat ini alias gratis.\n" +
+                            "Sebenarnya apabila dapat dikelola dengan baik Telaga Kumpe dapat dijadikan destinasi wisata andalan lain yang ada di Kecamatan Cilongok.",
+                    "https://jadwaltravel.com/wp-content/uploads/2019/08/Tiket-Masuk-Telaga-Kumpe.jpg"},
+            {"5",
+                    "Bukit Watu Meja",
+                    "Bukit Watu Meja Watu Meja ini berada di atas perbukitan di sepanjang jalan pintas Patikraja – Sampang melalui jembatan lama. Akses masuk dari jalan besar hanya bisa untuk sepeda motor sampai sebelum jalan setapak yang hanya bisa dilalui pejalan kaki. Dari jalan besar ada beberapa akses yang sudah ada tanda spanduk dan beberapa penjaga sekaligus tukang parkir.\n" +
+                            "Untuk masuk ke lokasi Watu Meja saat ini masih seikhlas pengunjung dan tidak tidak dipatok besaran minimalnya.",
+                    "https://pbs.twimg.com/media/DfUaNWFVQAADZzR.jpg"},
+            {"6",
+                    "Desa Wisata Ketenger",
+                    "Desa Wisata Ketenger terletak di kawasan Baturaden, yang merupakan sebuah kawasan wisata yang cukup popular dengan daya tarik yang mengagumkan. Ketika pelancong menuju desa ini, banyak aktraksi alam yang sangat indah seperti Curug (air terjun) Gede,\n" +
+                            "Curug Kembar, Curug Bayan, Curug Celiling, Curug Petir Curug Guman dan masih banyak lainnya.",
+                    "https://hotelmelakablog.files.wordpress.com/2017/04/curug-bayan-e1491213742200.jpg"},
+            {"7",
+                    "Goa Sarabadak",
+                    "Objek wisata alam yang satu ini sangat cocok untuk dikunjungi berada di Kabupaten Banyumas Jawa Tengah. Objek wisata alam tersebut bernama Goa Sarabadak. Goa Sarabadak  tersebut berada di Desa Kemutuk Lor Kecamatan Baturaden Kababupaten Banyumas Jawa Tengah atau sekitar 14 Kilometer dari pusat Kota Purwokerto.\n" +
+                            "Lokasi Objek wisata Gua Sarabadak masih berada di dalam kompleks Pancuran Pitu Baturraden",
+                    "https://hotelmelakablog.files.wordpress.com/2017/04/goa-sarabadak.jpg"},
+            {"8",
+                    "Bukit Gadog",
+                    "Bukit Sejuta Bintang atau tenar dengan sebutan Bukit Gadog terletak di Dusun Cibun, Desa Sunyalangu, Kecamatan Karanglewas Kabupaten Banyumas.\n" +
+                            "            Tempat wisata baru ini sedang dirintis oleh masyarakat di Dusun Cibun.",
+                    "https://cdn.yukepo.com/content-images/listicle-images/2017/11/25/72998.jpg"},
+            {"9",
+                    "Limpakuwus",
+                    "Desa Limpakuwus berada di Lembah Gunung Slamet. Membujur dari Puncak Gunung kearah Selatan sampai dengan perbatasan Desa Kotayasa Kecamatan Sumbang. Tanah Subur, Hutan Hijau, dengan penduduk yang semakin komplek dan beragam karakteristiknya. Itulah Desa Limpakuwus..namun kita tidak ingin membahas desa limpa kuwus secra detail kecuali di bagian Hutan Pinus nya\n" +
+                            "yang menjadi daya tarik tersendiri bagi para pemburu foto yang Instagramable.",
+                    "https://2.bp.blogspot.com/-xYu93_iMiRc/XIh7MbnhDyI/AAAAAAAAB4Q/k0a6Nl0SqHIedPpK8T1bGKxik7IUFoulQCLcBGAs/s1600/pns.jpg"}
+    };
+
+    public static ArrayList<Hmj> getListData(){
+        ArrayList<Hmj> list = new ArrayList<>();
+        for (String[] aData: data){
+            Hmj hmj = new Hmj();
+            hmj.setId(Integer.parseInt(aData[0]));
+            hmj.setName(aData[1]);
+            hmj.setDescription(aData[2]);
+            hmj.setPhoto(aData[3]);
+
+            list.add(hmj);
+        }
+        return list;
+    }
+}
